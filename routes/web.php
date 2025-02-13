e10db9f7316b44be1757a857d8030c63a759db23
abdul kader <akbd4807@gmail.com>
2025-02-13 13:20:52 +0600

    done

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,4 @@
 <?php
-
 use App\Http\Controllers\BrandController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\CustomerController;
@@ -7,25 +6,24 @@ use App\Http\Controllers\ManufacturerController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\StatusController;
 use App\Http\Controllers\SupplierController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
 Route::get('/', function () {
-    return view('dashboard');
+    return view('welcome');
+    // return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-// Route::get('customer',[CustomerController::class,'index']);
-
-// Route::prefix('customer')->group(function(){
-// Route::get('create',[CustomerController::class,'create']);
-// Route::post('create', [CustomerController::class,'store']);
-// Route::get('update/{id}',[CustomerController::class,'edit']);
-// Route::post('update',[CustomerController::class,'update']);
-// Route::get('delete/{id}',[CustomerController::class,'destroy_view']);
-// Route::post('delete',[CustomerController::class,'destroy']);
-// Route::post('customer/search',[CustomerController::class,'search']);
-// Route::get('show/{id}',[CustomerController::class,'show']);
-
-// });
 
 Route::get('customer/delete/{id}',[CustomerController::class,'destroy_view']);
 Route::post('customer/search',[CustomerController::class,'search']);
@@ -59,3 +57,11 @@ Route::get('brand/delete/{id}', [BrandController::class,'destroy_view']);
 Route::post('brand/search',[BrandController::class,'search']);
 Route::resource('brand',BrandController::class);
 
+
+
+
+
+
+
+
+require __DIR__.'/auth.php';
